1c4df6cc02b3da29d644d597b21b36367e7989eb
Seungyeon Han <tiffanyhansy@gmail.com>
2024-02-20 02:47:52 +0900

    feat: 마이페이지 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Signup from './pages/Signup/Signup';
 import Analysis from './pages/Analysis/Analysis';
 import Register from './pages/Register/Register';
 import Supplement from './pages/Supplement/Supplement';
+import Mypage from './pages/Mypage/Mypage.jsx';
+import Modify from './pages/Modify/Modify.jsx';
 import RegisterOwn from './pages/Register/RegisterOwn';
 import axios from 'axios';
 import { useEffect } from 'react';
 
 function App() {
-
   //서버 데이터 console 로 확인
   // useEffect(() => {
   //   axios.get('/api/test')
@@ -19,18 +20,20 @@ function App() {
   //     .catch()
   // })
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/users/login" element={<Login/>} />
-          <Route path="/main" element={<Main/>} />
-          <Route path="/users/join" element={<Signup/>} />
-          <Route path='/diet/analysis' element={<Analysis/>} />
-          <Route path='/diet/list' element={<Register />} />
-          <Route path='/supplement' element={<Supplement/>} />
-          <Route path='/diet/record' element={<RegisterOwn />} />
-          <Route path='/*' element={<Main />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/users/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/users/join" element={<Signup />} />
+        <Route path="/diet/analysis" element={<Analysis />} />
+        <Route path="/diet/list" element={<Register />} />
+        <Route path="/supplement" element={<Supplement />} />
+        <Route path="/diet/record" element={<RegisterOwn />} />
+        <Route path="/*" element={<Main />} />
+        <Route path="/users/mypage" element={<Mypage />} />
+        <Route path="/users/modify" element={<Modify />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
